3638710073b8626df6ff358480d9b89cae412a17
catiaraminhos <catia.s.raminhos@gmail.com>
2021-02-26 15:32:21 +0000

    Add navbar to HeaderComponent

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -22,4 +22,19 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show logo', () => {
+    const logoElement = fixture.nativeElement.querySelector('[data-test="logo"]');
+    expect(logoElement).toBeTruthy();
+  });
+
+  it('should show search', () => {
+    const searchElement = fixture.nativeElement.querySelector('[data-test="search"]');
+    expect(searchElement).toBeTruthy();
+  });
+
+  it('should show menu', () => {
+    const menuElement = fixture.nativeElement.querySelector('[data-test="menu"]');
+    expect(menuElement).toBeTruthy();
+  });
 });
