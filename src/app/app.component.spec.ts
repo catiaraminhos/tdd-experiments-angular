0bf1e13baa299698b8a2cd2240cbc7e9337fea01
catiaraminhos <catia.s.raminhos@gmail.com>
2021-02-26 14:43:50 +0000

    Add generated HeaderComponent and HomesComponent, and update tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,55 @@
-import { TestBed } from '@angular/core/testing';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 
+@Component({
+  selector: 'app-header',
+  template: ''
+})
+class HeaderStubComponent {
+}
+
+@Component({
+  selector: 'app-homes',
+  template: ''
+})
+class HomesStubComponent {
+}
+
 describe('AppComponent', () => {
+  let appFixture: ComponentFixture<AppComponent>;
+  let appComponent: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
-      ],
+        AppComponent,
+        HeaderStubComponent,
+        HomesStubComponent
+      ]
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    appFixture = TestBed.createComponent(AppComponent);
+    appComponent = appFixture.componentInstance;
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(appComponent).toBeTruthy();
   });
 
-  it(`should have as title 'tdd-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tdd-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tdd-angular app is running!');
+  it('should have a header and homes', () => {
+    appFixture.detectChanges();
+    const headerComponent = appFixture.debugElement.query(By.directive(HeaderStubComponent));
+    const homesComponent = appFixture.debugElement.query(By.directive(HomesStubComponent));
+    expect(headerComponent).toBeTruthy();
+    expect(homesComponent).toBeTruthy();
   });
 });
