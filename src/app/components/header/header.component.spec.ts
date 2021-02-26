4562a4a29fc9d3fe155e76ca18f4ee3b6621aa54
catiaraminhos <catia.s.raminhos@gmail.com>
2021-02-26 15:50:47 +0000

    Refactor HeaderComponent tests

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -24,33 +24,28 @@ describe('HeaderComponent', () => {
   });
 
   it('should show logo', () => {
-    const logoElement = fixture.nativeElement.querySelector('[data-test="logo"]');
-    expect(logoElement).toBeTruthy();
+    expectTestElementIsDefined('logo');
   });
 
   it('should show search', () => {
-    const searchElement = fixture.nativeElement.querySelector('[data-test="search"]');
-    expect(searchElement).toBeTruthy();
+    expectTestElementIsDefined('search');
   });
 
   it('should show menu', () => {
-    const menuElement = fixture.nativeElement.querySelector('[data-test="menu"]');
-    expect(menuElement).toBeTruthy();
+    expectTestElementIsDefined('menu');
   });
 
   it('should show filters', () => {
-    const homeTypeFilterElem = fixture.nativeElement.querySelector('[data-test="home-type"]');
-    const datesFilterElem = fixture.nativeElement.querySelector('[data-test="dates"]');
-    const guestsFilterElem = fixture.nativeElement.querySelector('[data-test="guests"]');
-    const priceFilterElem = fixture.nativeElement.querySelector('[data-test="price"]');
-    const roomsFilterElem = fixture.nativeElement.querySelector('[data-test="rooms"]');
-    const amenitiesFilterElem = fixture.nativeElement.querySelector('[data-test="amenities"]');
-
-    expect(homeTypeFilterElem).toBeTruthy();
-    expect(datesFilterElem).toBeTruthy();
-    expect(guestsFilterElem).toBeTruthy();
-    expect(priceFilterElem).toBeTruthy();
-    expect(roomsFilterElem).toBeTruthy();
-    expect(amenitiesFilterElem).toBeTruthy();
+    expectTestElementIsDefined('home-type');
+    expectTestElementIsDefined('dates');
+    expectTestElementIsDefined('guests');
+    expectTestElementIsDefined('price');
+    expectTestElementIsDefined('rooms');
+    expectTestElementIsDefined('amenities');
   });
+
+  function expectTestElementIsDefined(name: string): void {
+    const testElement = fixture.nativeElement.querySelector('[data-test="' + name + '"]');
+    expect(testElement).toBeTruthy();
+  }
 });
