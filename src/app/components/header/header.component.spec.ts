3e3b2a4f0b73c0a27e818a0d4b8f21a47e704923
Cátia Raminhos <catia.s.raminhos@gmail.com>
2021-02-26 16:53:14 +0000

    Add application header (#3)
    * Add generated HeaderComponent and HomesComponent, and update tests
    
    * Add jquery uikit to dev dependencies
    
    * Add jquery ui scripts to Angular project
    
    * Import styles from jquery ui
    
    * Add navbar to HeaderComponent
    
    * Add filters to HeaderComponent
    
    * Refactor HeaderComponent tests

---
+++ src/app/components/header/header.component.spec.ts
@@ -0,0 +1,51 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HeaderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should show logo', () => {
+    expectTestElementIsDefined('logo');
+  });
+
+  it('should show search', () => {
+    expectTestElementIsDefined('search');
+  });
+
+  it('should show menu', () => {
+    expectTestElementIsDefined('menu');
+  });
+
+  it('should show filters', () => {
+    expectTestElementIsDefined('home-type');
+    expectTestElementIsDefined('dates');
+    expectTestElementIsDefined('guests');
+    expectTestElementIsDefined('price');
+    expectTestElementIsDefined('rooms');
+    expectTestElementIsDefined('amenities');
+  });
+
+  function expectTestElementIsDefined(name: string): void {
+    const testElement = fixture.nativeElement.querySelector('[data-test="' + name + '"]');
+    expect(testElement).toBeTruthy();
+  }
+});
