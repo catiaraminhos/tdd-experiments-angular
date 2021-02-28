4899306c38d436f395e140a8ef4ef14ffbeb3431
catiaraminhos <catia.s.raminhos@gmail.com>
2021-02-28 15:26:31 +0000

    Setup Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +17,9 @@ import { HomesComponent } from './components/homes/homes.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
+    MatDialogModule,
     AppRoutingModule
   ],
   providers: [],
